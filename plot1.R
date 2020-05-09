9fbadf6c6186c86b4b600bced9986ae82c130342
Rohan Jha <48327725+rohanjha613@users.noreply.github.com>
2020-05-09 23:23:20 +0530

    files added

---
+++ plot1.R
@@ -0,0 +1,18 @@
+library(data.table)
+#Reads in data from file then subsets data for specified dates
+data <- data.table::fread(input = "household_power_consumption.txt"
+                             , na.strings="?")
+
+# Change Date Column to Date Type
+data[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+
+# Filter Dates for 2007-02-01 and 2007-02-02
+sub_data <- data[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+
+png("plot1.png", width=480, height=480)
+
+## Plot 1
+hist(sub_data[, Global_active_power], main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,35 @@
+library(data.table)
+#Reads in data from file then subsets data for specified dates
+data <- data.table::fread(input = "household_power_consumption.txt"
+                             , na.strings="?")
+
+# Making a POSIXct date capable of being filtered and graphed by time of day
+data[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+# Filter Dates for 2007-02-01 and 2007-02-02
+sub_data <- data[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("plot4.png", width=480, height=480)
+
+par(mfrow=c(2,2))
+
+# Plot 1
+plot(sub_data[, dateTime], sub_data[, Global_active_power], type="l", xlab="", ylab="Global Active Power")
+
+# Plot 2
+plot(sub_data[, dateTime],sub_data[, Voltage], type="l", xlab="datetime", ylab="Voltage")
+
+# Plot 3
+plot(sub_data[, dateTime], sub_data[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(sub_data[, dateTime], sub_data[, Sub_metering_2], col="red")
+lines(sub_data[, dateTime], sub_data[, Sub_metering_3],col="blue")
+legend("topright", col=c("black","red","blue")
+       , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
+       , lty=c(1,1)
+       , bty="n"
+       , cex=.5) 
+
+# Plot 4
+plot(sub_data[, dateTime], sub_data[,Global_reactive_power], type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
